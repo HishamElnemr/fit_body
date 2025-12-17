aef27312d2540d1d0995b7bdbe443914aa6cc0bc
HishamElnemr <hishammelnmr@gmail.com>
2025-12-17 18:19:29 +0200

    Initialize project with Plus Jakarta Sans font, core styles, colors, and basic app structure.

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryWhite = Color(0xFFFFFFFF);
-  static const Color primaryBlack = Color(0xFF000000);
-  static const Color primaryLimeGreen = Color(0xFFE2F163);
-  static const Color primaryPurple = Color(0xFF896CFE);
-  static const Color primaryLightPurple = Color(0xFFB3A0FF);
+  static const Color black = Color(0xFF1C1B1B);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color cyan = Color(0xFF21D4B4);
+  static const Color cyan50 = Color(0xFFF4FDFA);
+  static const Color grey100 = Color(0xFFC0C0C0);
+  static const Color red = Color(0xFFEE4D4D);
+  static const Color blue = Color(0xFF1F88DA);
+  static const Color purple = Color(0xFF4F1FDA);
+  static const Color yellow = Color(0xFFEBEF14);
+  static const Color orange = Color(0xFFF0821D);
+  static const Color merigold = Color(0xFFFFCB45);
+  static const Color brown = Color(0xFF5A1A05);
+  static const Color pink = Color(0xFFCE1DEB);
+
+  static const Color lightGrey50 = Color(0xFFF4F5FD);
+  static const Color lightGrey150 = Color(0xFF6F7384);
+
+  static const Color darkCyan50 = Color(0xFF212322);
+  static const Color darkGrey50 = Color(0xFF282828);
+  static const Color darkGrey150 = Color(0xFFA2A2A6);
 }
--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -1,197 +1,157 @@
 import 'package:flutter/material.dart';
 
 abstract class AppStyles {
-  static TextStyle styleBold20(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 20),
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w700, // Bold
-    );
-  }
+  static TextStyle heading1Bold32(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 32),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w700,
+    height: 1.2,
+    letterSpacing: 0,
+  );
 
-  static TextStyle styleBold18(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 18),
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w700,
-    );
-  }
+  static TextStyle heading1SemiBold32(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 32),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+    letterSpacing: 0,
+  );
 
-  static TextStyle styleMedium20(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 20),
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w500,
-    );
-  }
+  static TextStyle heading1Regular32(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 32),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w400,
+    height: 1.2,
+    letterSpacing: 0,
+  );
 
-  static TextStyle styleMedium18(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 18),
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w500,
-    );
-  }
+  static TextStyle heading2Bold24(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 24),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w700,
+    height: 1.2,
+    letterSpacing: 0,
+  );
 
-  static TextStyle styleMedium16(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 16),
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w500,
-    );
-  }
+  static TextStyle heading2SemiBold24(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 24),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+    letterSpacing: 0,
+  );
 
-  static TextStyle styleMedium14(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 14),
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w500,
-    );
-  }
+  static TextStyle heading2Regular24(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 24),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w400,
+    height: 1.2,
+    letterSpacing: 0,
+  );
 
-  static TextStyle styleMedium13(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 13),
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w500,
-    );
-  }
+  static TextStyle heading3Bold18(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 18),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w700,
+    height: 1.2,
+    letterSpacing: 0.0025,
+  );
 
-  static TextStyle styleMedium12(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 12),
-      fontFamily: 'Poppins',
-      fontWeight: FontWeight.w500,
-    );
-  }
+  static TextStyle heading3SemiBold18(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 18),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+    letterSpacing: 0.0025,
+  );
 
-  static TextStyle styleRegular18(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 18),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w400,
-    );
-  }
+  static TextStyle heading3Regular18(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 18),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w400,
+    height: 1.2,
+    letterSpacing: 0.0025,
+  );
 
-  static TextStyle styleRegular17(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 17),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w400,
-    );
-  }
+  static TextStyle button1SemiBold16(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 16),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w600,
+    height: null,
+    letterSpacing: 0,
+  );
 
-  static TextStyle styleRegular15(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 15),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w400,
-    );
-  }
+  static TextStyle button2SemiBold14(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 14),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w600,
+    height: null,
+    letterSpacing: 0,
+  );
 
-  static TextStyle styleRegular14(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 14),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w400,
-    );
-  }
+  static TextStyle body1Medium16(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 16),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+    letterSpacing: 0.005,
+  );
 
-  static TextStyle styleRegular13(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 13),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w400,
-    );
-  }
+  static TextStyle body1Regular16(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 16),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: 0.005,
+  );
 
-  static TextStyle styleRegular12(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 12),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w400,
-    );
-  }
+  static TextStyle body2Medium14(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 14),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+    letterSpacing: 0.005,
+  );
 
-  static TextStyle styleLight14(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 14),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w300,
-    );
-  }
+  static TextStyle body2Regular14(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 14),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+    letterSpacing: 0.005,
+  );
 
-  static TextStyle styleLight13(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 13),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w300,
-    );
-  }
+  static TextStyle captionSemiBold12(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 12),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w600,
+    height: null,
+    letterSpacing: 0.005,
+  );
 
-  static TextStyle styleLight12(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 12),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w300,
-    );
-  }
+  static TextStyle captionRegular12(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 12),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w400,
+    height: null,
+    letterSpacing: 0.005,
+  );
 
-  static TextStyle styleSemiBold15(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 15),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w600,
-    );
-  }
+  static TextStyle overlineSemiBold10(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 10),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w600,
+    height: null,
+    letterSpacing: 0.015,
+  );
 
-  static TextStyle styleSemiBold14(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 14),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w600,
-    );
-  }
-
-  static TextStyle styleSemiBold13(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 13),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w600,
-    );
-  }
-
-  static TextStyle styleSemiBold12(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 12),
-      fontFamily: 'LeagueSpartan',
-      fontWeight: FontWeight.w600,
-    );
-  }
-
-  static TextStyle styleInterBold14(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 14),
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w700,
-    );
-  }
-
-  static TextStyle styleInterBold13(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 13),
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w700,
-    );
-  }
-
-  static TextStyle styleInterBold12(BuildContext context) {
-    return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 12),
-      fontFamily: 'Inter',
-      fontWeight: FontWeight.w700,
-    );
-  }
+  static TextStyle overlineRegular10(BuildContext context) => TextStyle(
+    fontSize: getResponsiveFontSize(context, fontSize: 10),
+    fontFamily: 'Plus Jakarta Sans',
+    fontWeight: FontWeight.w400,
+    height: null,
+    letterSpacing: 0.015,
+  );
 }
 
 double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
